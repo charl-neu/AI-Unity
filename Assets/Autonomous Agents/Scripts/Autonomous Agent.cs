6746042f84a7a5fcf3d0c62885be96b8b158293e
charl-neu <cshenkir@student.neumont.edu>
2026-01-21 10:27:20 -0700

    fix wandering

--- Assets/Autonomous Agents/Scripts/Autonomous Agent.cs
+++ Assets/Autonomous Agents/Scripts/Autonomous Agent.cs
@@ -8,19 +8,31 @@ public class AutonomousAgent : AIAgent
     [SerializeField] Perception seekperception;
     [SerializeField] Perception fleeperception;
 
+    [Header("Wander")]
+    [SerializeField] float wanderRadius = 1;
+    [SerializeField] float wanderDistance = 1;
+    [SerializeField] float wanderDisplacement = 1;
+
+
+
+    float wanderAngle = 0.0f;
+
 
     void Start()
     {
-
+        wanderAngle = Random.Range(0, 360);
     }
 
     void Update()
     {
+        bool hasTarget = false;
+
         if (seekperception != null)
         {
             var gameObjects = seekperception.GetGameObjects();
             if (gameObjects.Length > 0)
             {
+                hasTarget = true;
                 Vector3 force = Seek(gameObjects[0]);
                 movement.ApplyForce(force);
             }
@@ -30,6 +42,7 @@ public class AutonomousAgent : AIAgent
             var gameObjects = fleeperception.GetGameObjects();
             if (gameObjects.Length > 0)
             {
+                hasTarget = true;
                 Vector3 force = Flee(gameObjects[0]);
                 movement.ApplyForce(force);
             }
@@ -39,37 +52,33 @@ public class AutonomousAgent : AIAgent
         //    Debug.DrawLine(transform.position, obj.transform.position, Color.red);
         //}
 
-
-
-        transform.position = Utilities.Wrap(transform.position, new Vector3(-25,-25,-25), new Vector3(25,25,25));
-
-        if (movement.Velocity.sqrMagnitude == 0)
+        if (!hasTarget)
         {
             //use getting started force in combination with random wandering if the agent is not moving. we want to avoid agents doing nothing.
-            Vector3 randomDirection = Quaternion.Euler(0, Random.Range(-15f, 15f), 0) * transform.forward; // random direction in xz plane facing mostly in the direction the agent is facing
-            Vector3 direction = Vector3.Lerp(movement.Velocity.normalized, randomDirection.normalized, 0.3f).normalized * movement.maxSpeed; // slightly adjust current velocity towards random direction
-            Vector3 force = GetStartingForce(direction);
+            Vector3 force = Wander();
             movement.ApplyForce(force);
         }
 
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-25,-25,-25), new Vector3(25,25,25));
+        
         transform.rotation = Quaternion.LookRotation(movement.Velocity);
     }
 
     Vector3 Seek(GameObject go)
     {
         Vector3 direction = go.transform.position - transform.position;
-        Vector3 force = GetStartingForce(direction);
+        Vector3 force = GetSteeringForce(direction);
         return force;
     }
 
     Vector3 Flee(GameObject go)
     {
         Vector3 direction = transform.position - go.transform.position;
-        Vector3 force = GetStartingForce(direction);
+        Vector3 force = GetSteeringForce(direction);
         return force;
     }
 
-    Vector3 GetStartingForce(Vector3 direction)
+    Vector3 GetSteeringForce(Vector3 direction)
     {
         Vector3 desired = direction.normalized * movement.maxSpeed;
         Vector3 steer = desired - movement.Velocity;
@@ -77,4 +86,29 @@ public class AutonomousAgent : AIAgent
         return force;
     }
 
+    private Vector3 Wander()
+
+    {
+
+        // randomly adjust the wander angle within (+/-) displacement range 
+
+        wanderAngle += Random.Range(-wanderDisplacement, wanderDisplacement);
+
+        // calculate a point on the wander circle using the wander angle 
+
+        Quaternion rotation = Quaternion.AngleAxis(wanderAngle, Vector3.up);
+
+        Vector3 pointOnCircle = rotation * Vector3.forward * wanderRadius;
+
+        // project the wander circle in front of the agent 
+
+        Vector3 circleCenter = movement.Velocity.normalized * wanderDistance;
+
+        // steer toward the target point (circle center + point on circle) 
+
+        Vector3 force = GetSteeringForce(circleCenter + pointOnCircle);
+
+        return force;
+
+    }
 }
